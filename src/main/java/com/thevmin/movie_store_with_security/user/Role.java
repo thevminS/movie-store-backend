54a9eb1ead9881b5d53a43a6f8949013daaf2e75
Thevmin Sanhinda <thevmin.s@intervest.lk>
2024-07-09 10:58:01 +0530

    Made the role serializable

--- src/main/java/com/thevmin/movie_store_with_security/user/Role.java
+++ src/main/java/com/thevmin/movie_store_with_security/user/Role.java
@@ -8,12 +8,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-public class Role {
+public class Role implements Serializable {
     @Id
     @GeneratedValue
     private Long id;
